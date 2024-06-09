ea664ca5f4883ad58752319c7432d982eab96318
VIRENDER KUMAR <A1WRSV9Q@airtel.com>
2024-06-09 10:34:27 +0530

    ek or rg

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rgs" {
   name     = "rg1"
   location = "central india"
 }
+
+resource "azurerm_resource_group" "rgnew" {
+  name     = "rg2"
+  location = "central india"
+}
